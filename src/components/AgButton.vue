e01b53ef4f2e4a11d1a01227be35be8cc6d9d887
henriquevc <henriquevc93@gmail.com>
2021-10-21 16:48:08 -0300

    criando componentes de button e input

---
+++ src/components/AgButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button
+        :class="`bg-${color}-800 hover:bg-${color}-600 border-none focus:border-indigo-700 outline-none text-lg text-white py-2 px-4 sm:py-3 sm:px-6 font-bold rounded-lg focus:ring focus:ring-${color}-500 focus:ring-offset-1 disabled:opacity-70`"
+    >
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        label: String,
+        color: {
+            String,
+            default: 'gray'
+        }
+    },
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
---
+++ src/components/AgInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <input
+        :id="id"
+        :type="type"
+        label="Host"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        class="font-semibold border rounded-md pl-4 py-3 focus:ring-grey-500"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            required: true,
+            default: 'text'
+        },
+        modelValue: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: false
+        }
+    }
+}
+</script>
